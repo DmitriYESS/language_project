2550e26b2f14250851bdeb21392ca49546df1d02
DmitriYESS <124189774+DmitriYESS@users.noreply.github.com>
2023-04-30 01:42:17 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
 
 
-def test_language_and_add_button(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     time.sleep(5)
     try:
