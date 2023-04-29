7efc7d74cda1281de81c6a44d3ad93902b5db4e7
DmitriYESS <dpetrakov97@gmail.com>
2023-04-30 01:13:58 +0300

    adding assert

--- test_items.py
+++ test_items.py
@@ -1,10 +1,17 @@
 import time
-
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
 
 
 def test_language_and_add_button(browser):
     browser.get(link)
     time.sleep(5)
-    browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    try:
+        button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        assert button.is_displayed()
+        print("Кнопка найдена и отображается на странице")
+
+    except NoSuchElementException:
+        print("Кнопка не найдена на странице")
